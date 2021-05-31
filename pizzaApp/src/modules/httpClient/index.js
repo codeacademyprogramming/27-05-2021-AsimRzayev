f44af4065104f395135f7ebf27cdeaf588f48de3
AsimRzayev <asimrr@code.edu.az>
2021-05-31 05:40:46 +0400

    AddHTTPCLIENT

---
+++ pizzaApp/src/modules/httpClient/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export class HttpClient {
+
+
+    async get(url) {
+        const res = await axios.get(`${url}`);
+
+        return res.data;
+    }
+
+    async post(url, body) {
+        return await axios.post(`${url}`, body);
+    }
+}
