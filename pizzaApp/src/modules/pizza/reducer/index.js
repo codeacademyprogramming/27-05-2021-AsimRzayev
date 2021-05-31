a0d1f779ff707f108d2f617cb293401bdf39370a
AsimRzayev <asimrr@code.edu.az>
2021-05-31 05:41:16 +0400

    ADDPIZZAMODULE

---
+++ pizzaApp/src/modules/pizza/reducer/index.js
@@ -0,0 +1,38 @@
+import { ASYNC_STATUS } from "../../../redux/consts";
+import { PIZZA_ACTIONS } from "../actions/consts";
+
+const initialState = {
+    data: [],
+    error: null,
+    status: ASYNC_STATUS.IDLE
+};
+
+export function pizzaReducer(state = initialState, action) {
+    switch (action.type) {
+        case PIZZA_ACTIONS.GET_PIZZAS:
+            return {
+                ...state,
+                status: ASYNC_STATUS.LOADING,
+                data: [],
+                error: null,
+            };
+        case `${PIZZA_ACTIONS.GET_PIZZAS}_SUCCESS`:
+        return {
+                
+                ...state,
+                status: ASYNC_STATUS.SUCCESS,
+                data: action.payload,
+                error: null,
+            };
+        case `${PIZZA_ACTIONS.GET_PIZZAS}_ERROR`:
+            return {
+                ...state,
+                status: ASYNC_STATUS.ERROR,
+                data: [],
+                error: action.error
+            };
+        default:
+            break;
+    }
+    return state;
+}
